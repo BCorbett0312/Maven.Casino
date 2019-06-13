562b58aa76bcceeb041f7481b31c188325b643d2
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-12 23:24:02 -0400

    FieldBet tests added and passing

--- src/main/java/io/zipcoder/casino/craps/FieldBet.java
+++ src/main/java/io/zipcoder/casino/craps/FieldBet.java
@@ -2,24 +2,47 @@ package io.zipcoder.casino.craps;
 
 public class FieldBet implements CrapsBet {
 
+    private Integer value;
     private Integer payoutMultiplier;
+    private Boolean won;
+    private Boolean lost;
 
     public FieldBet(Integer value){
+        this.value = value;
+        this.payoutMultiplier = null;
+        this.won = false;
+        this.lost = false;
     }
 
     public Integer payout() {
-        return null;
+        if(won && !lost){
+            return value * payoutMultiplier;
+        }
+        else {
+            return null;
+        }
     }
 
     public void checkRoll(CrapsRoll roll) {
+        if(roll.getValue() == 2 || roll.getValue() == 12){
+            won = true;
+            payoutMultiplier = 3;
+        }
+        else if(roll.getValue() >= 5 && roll.getValue() <= 8){
+            lost = true;
+        }
+        else{
+            won = true;
+            payoutMultiplier = 2;
+        }
 
     }
 
     public Boolean hasLost() {
-        return null;
+        return lost;
     }
 
     public Boolean hasWon() {
-        return null;
+        return won;
     }
 }
