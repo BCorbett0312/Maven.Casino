61f9e193443c04317c4d5a50395d8595264dcf05
sputnika <sputnika91@gmail.com>
2019-06-16 18:39:30 -0400

    game almost running

--- src/main/java/io/zipcoder/casino/GoFish/GoFish.java
+++ src/main/java/io/zipcoder/casino/GoFish/GoFish.java
@@ -5,9 +5,10 @@ import io.zipcoder.casino.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
 
 import io.zipcoder.casino.utilities.Console;
-
+import org.graalvm.compiler.lir.amd64.AMD64Binary;
 
 
 public class GoFish extends CardGame {
@@ -18,10 +19,18 @@ public class GoFish extends CardGame {
     private GoFishPlayer playerA;
     private GoFishPlayer comp;
     private Hand compHand;
+    GoFishPlayer asking;
+    GoFishPlayer giving;
+
+
     Integer numBook;
+    Integer playerBooks;
+    Integer compBooks;
     Boolean gameOver;
     Boolean playing;
-    Boolean switchTurns;
+
+
+    String number = "";
 
     Console console = new Console(System.in, System.out);
 
@@ -35,6 +44,8 @@ public class GoFish extends CardGame {
     }
 
 
+
+
     public void newGame(){
         playing = true;
         gameOver=false;
@@ -43,14 +54,55 @@ public class GoFish extends CardGame {
         checkBooks(compHand);
     }
 
-    public void startGame (){
-        newGame();
-        while(playing){
-            console.getStringInput("Do you want to play Go Fish?");
-
+    public void switchPartner() {
+        if (asking == playerA) {
+            asking = comp;
+            giving = playerA;
+        } else {
+            asking =playerA;
+            giving = comp;
         }
+
     }
 
+
+    public void startGame (){
+        String input = console.getStringInput("Do you want to play Go Fish? Enter yes or no");
+         switch(input){
+             case "yes" :
+                 rules();
+                 break;
+             case "no" :
+                 //exit
+                 break;
+         }
+        newGame();
+
+         asking = playerA;
+         giving=comp;
+
+         while(playing){
+
+            askedCard(number, giving.getHand(),asking.getHand());
+            checkBooks(asking.getHand());
+             checkHand(asking);
+             checkDeck();
+            switchPartner();
+
+         }
+    }
+
+
+
+    public String getGuess(){
+        if(asking == playerA){
+         number = console.getStringInput("It's your turn. Here are your cards. What card do you want?" +
+                "\n Enter the card number. ace,two,three,four,five,six,seven,eight,nine,ten,jack,queen,king");
+        return number;}
+       else {
+            return null;
+
+    }}
 
 
 
@@ -93,24 +145,124 @@ public class GoFish extends CardGame {
     }
 
     public void rules () {
-        console.println("You and your opponent will each receive 7 cards each. When it is your turn, you can choose a card and ask your opponent if they have a card of that value");
-        console.println("If they do, they'll give them to you. If not, you may take a card from the deck. Then it is your opponent's turn.");
-        console.println("A book is when you have four cards of the same value. The player with the most books wins. The game ends when either your run out of cards in your hand ");
-        console.println("or the deck runs out of cards. Enjoy the game and maybe you will even win!");
+        console.println("You and your opponent will each receive 7 cards each." +
+        "\nWhen it is your turn, you can choose a card and ask your opponent if they have a card of that value"+
+        "\nIf they do, they'll give them to you. If not, you may take a card from the deck."+
+                "\nThen it is your opponent's turn."+
+        "\nA book is when you have four cards of the same value. The player with the most books wins."+
+                "\nThe game ends when either your run out of cards in your hand or the deck runs out of cards."
+                +"\nEnjoy the game and maybe you will even win!");
 
     }
 
-    public void playerFromDeck(){
-        playerA.getHand().add(deck.draw());
+    public void playerFromDeck(Hand askingHand){
+        askingHand.add(deck.draw());
     }
 
-    public void compFromDeck(){
-        comp.getHand().add(deck.draw());
-    }
+
+
 
     public void showCards(){
         handPlayerA.toString();
     }
+
+    public void passCard(Hand giving,Hand asking,CardValue value) {
+            Boolean hadCard = false;
+            Card card1 = new Card(CardSuit.CLUB,value);
+         Card card2= new Card(CardSuit.SPADE,value);
+         Card card3=new Card(CardSuit.DIAMOND,value);
+            Card card4 = new Card(CardSuit.HEART,value);
+            if(giving.contains(card1)){
+                giving.removeByCard(card1);
+             asking.add(card1);
+            hadCard=true;
+            }
+            if(giving.contains(card2)){
+                giving.removeByCard(card2);
+                asking.add(card2);
+            hadCard=true;
+            }
+        if(giving.contains(card3)){
+            giving.removeByCard(card3);
+            asking.add(card3);
+            hadCard=true;
+        }
+        if(giving.contains(card4)){
+            giving.removeByCard(card4);
+            asking.add(card4);
+            hadCard=true;
+        }
+        if(hadCard == false){
+            console.println("GO FISH!");
+            playerFromDeck(asking);
+
+        }
+    }
+
+    public void askedCard (String number, Hand givingHand, Hand askingHand) {
+    switch(number){
+        case "ace" :
+            passCard(givingHand,askingHand,CardValue.ACE);
+            break;
+        case "two" :
+            passCard(givingHand,askingHand, CardValue.TWO);
+            break;
+        case "three" :
+            passCard(givingHand,askingHand,CardValue.THREE);
+            break;
+        case "four":
+            passCard(givingHand,askingHand,CardValue.FOUR);
+            break;
+        case "five":
+            passCard(givingHand,askingHand,CardValue.FIVE);
+            break;
+        case "six":
+            passCard(givingHand,askingHand,CardValue.SIX);
+            break;
+        case "seven":
+            passCard(givingHand,askingHand,CardValue.SEVEN);
+            break;
+        case "eight":
+            passCard(givingHand,askingHand,CardValue.EIGHT);
+            break;
+        case "nine":
+            passCard(givingHand,askingHand,CardValue.NINE);
+            break;
+        case "ten":
+            passCard(givingHand,askingHand,CardValue.TEN);
+            break;
+        case "jack":
+            passCard(givingHand,askingHand,CardValue.JACK);
+            break;
+        case "queen":
+            passCard(givingHand,askingHand,CardValue.QUEEN);
+            break;
+        case "king":
+            passCard(givingHand,askingHand,CardValue.KING);
+            break;
+    }}
+
+    public void goFish (Hand giving,Hand asking,,CardValue value){
+        Card card1 = new Card(CardSuit.CLUB,value);
+        Card card2= new Card(CardSuit.SPADE,value);
+        Card card3=new Card(CardSuit.DIAMOND,value);
+        Card card4 = new Card(CardSuit.HEART,value);
+        if(!(giving.contains(card1))){
+            console.println("GO FISH!");
+            playerFromDeck(asking);
+        }
+        if(!(giving.contains(card2))){
+            console.println("GO FISH!");
+            playerFromDeck(asking);}
+        if(!(giving.contains(card3))){
+            console.println("GO FISH!");
+            playerFromDeck(asking);}
+        if(!(giving.contains(card4)){
+            console.println("GO FISH!");
+            playerFromDeck(asking);}
+    }
+
+
 
 
 
